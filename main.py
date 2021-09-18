f52514779ce9095d323636d607cf50ea375b8746
Gabriel Pery <56797835+Xephire@users.noreply.github.com>
2021-09-18 12:44:48 +0000

    Added extra VCs to member count in config.py

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from discord.ext import commands
+import discord
 from utils import config
 from utils import keep_alive
 import os
 
-bot = commands.Bot(command_prefix=config.prefix, activity=config.starting_activity)
+bot = commands.Bot(command_prefix=config.prefix, activity=config.starting_activity, intents=discord.Intents.all())
 bot.remove_command("help")
 
 for filename in os.listdir("./cogs"):
