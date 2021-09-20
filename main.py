9200d4314d30c1fa73003daa93fb77b6a442216a
Gabriel Pery <gabrielperyy@gmail.com>
2021-09-20 21:49:39 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,13 +4,11 @@ from utils import config
 from utils import keep_alive
 import os
 
-bot = commands.Bot(command_prefix=config.prefix, activity=config.starting_activity, intents=discord.Intents.all())
-
-bot.remove_command("help")
+bot = commands.Bot(command_prefix=config.prefix, activity=config.starting_activity, intents=discord.Intents.all(), help_command=None)
 
 for filename in os.listdir("./cogs"):
     if filename.endswith(".py"):
-         bot.load_extension(f"cogs.{filename[:-3]}")
+        bot.load_extension(f"cogs.{filename[:-3]}")
 
 TOKEN = os.environ.get("TOKEN")
 
