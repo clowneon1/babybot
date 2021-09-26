fc649798aea55bad18d5bc239151c82b7500784e
Gabriel Pery <56797835+Xephire@users.noreply.github.com>
2021-09-26 13:36:23 +0000

    Changed the "clear" cog to use embed instead of regular text messages, changed timings as well.

--- cogs/clear.py
+++ cogs/clear.py
@@ -1,4 +1,5 @@
 from discord.ext import commands
+import discord
 import asyncio
 
 class Clear(commands.Cog):
@@ -10,27 +11,31 @@ class Clear(commands.Cog):
     async def clear(self, ctx, amount : int = None):
         global confirmMsgID, confirmMsgUserID
 
-        if ctx.author.guild_permissions.manage_messages:
-            if amount is None:
-                confirmMsg = await ctx.send('React to this message with :no_entry_sign: to confirm clearing the whole text channel. (This message automatically deletes after 5 seconds)', delete_after=5)
-                await confirmMsg.add_reaction('\N{NO ENTRY SIGN}')
-                confirmMsgID = confirmMsg.id
-                confirmMsgUserID = ctx.author.id
-                await asyncio.sleep(0.5)
-                return
-            
-            if isinstance(amount, int) and amount > 0:
-                await ctx.channel.purge(limit=amount+1)
-                delMsg = await ctx.send(f'**{amount}** messages were deleted')
-                await asyncio.sleep(2)
-                await delMsg.delete()
-                await asyncio.sleep(0.5)
-            else:
-                return await ctx.send('Please specify a valid number of messages to delete')
+        if amount is None:
+            em = discord.Embed(title="React to this message with :no_entry_sign: to confirm clearing the whole text channel. (This message automatically deletes after 5 seconds)",
+                colour=discord.Color.purple())
+            em.set_footer(text=f"Requested by {ctx.author.name}", icon_url=ctx.author.avatar_url)
+
+            confirmMsg = await ctx.send(embed=em)
+            await confirmMsg.add_reaction('\N{NO ENTRY SIGN}')
+            confirmMsgID = confirmMsg.id
+            confirmMsgUserID = ctx.author.id
+            return
+        
+        if isinstance(amount, int) and amount > 0:
+            await ctx.channel.purge(limit=amount+1)
+
+            em = discord.Embed(title=f"**{amount}** messages were deleted", colour=discord.Color.purple())
+            em.set_footer(text=f"Requested by {ctx.author.name}", icon_url=ctx.author.avatar_url)
+
+            delMsg = await ctx.send(embed=em)
+            await asyncio.sleep(3)
+            await delMsg.delete()
         else:
-            permErrorMsg = await ctx.send('You do not have permission to use this command')
-            await asyncio.sleep(2)
-            await permErrorMsg.delete()
+            em = discord.Embed(title=f"{ctx.author.mention} please specify a valid number of messages to delete", colour=discord.Color.purple())
+            em.set_footer(text=f"Requested by {ctx.author.name}", icon_url=ctx.author.avatar_url)
+
+            return await ctx.send(embed=em)
             
     @commands.Cog.listener()
     async def on_reaction_add(self, reaction, user):
@@ -38,9 +43,12 @@ class Clear(commands.Cog):
         except NameError: return
         if reaction.message.id == confirmMsgID and reaction.emoji == '\N{NO ENTRY SIGN}' and user.id != self.bot.user.id and user.id == confirmMsgUserID:
             await reaction.message.channel.purge(limit=None)
-            await asyncio.sleep(0.5)
-            delMsg = await reaction.message.channel.send('This channel was cleared')
-            await asyncio.sleep(1)
+
+            em = discord.Embed(title=f"This channel was cleared", colour=discord.Color.purple())
+            em.set_footer(text=f"Requested by {user.name}", icon_url=user.avatar_url)
+
+            delMsg = await reaction.message.channel.send(embed=em)
+            await asyncio.sleep(3)
             return await delMsg.delete()
 
 def setup(bot):
