084b4f6e3f157327ef118391211099e192175c95
Gabriel Pery <gabrielperyy@gmail.com>
2021-10-12 14:10:32 +0100

    Update main.py
    Changed how requirements from the utils directory are pulled

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-from utils import keep_alive, bot
+from utils.keep_alive import keep_alive
+from utils.bot import Bot
 import os
 
-bot = bot.Bot()
+
+bot = Bot()
 TOKEN = os.environ.get("TOKEN")
 
 def main():
-    keep_alive.keep_alive()
+    keep_alive()
     bot.run(TOKEN)
 
 if __name__=="__main__":
