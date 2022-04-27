8b608e4461300762b807237badd7e9afe69ed157
Yashraj Mandloi <67321447+clowneon1@users.noreply.github.com>
2022-04-27 09:01:40 +0000

    everything.

--- main.py
+++ main.py
@@ -1,13 +1,26 @@
-from utils.keep_alive import keep_alive
-from utils.bot import Bot
+import asyncio
+from contextlib import AsyncExitStack
+from dis import disco
+from http import client
+from sys import prefix
+import discord
 import os
+from discord.ext import commands, tasks
+from itertools import cycle
+import random
+import youtube_dl
+import time
+import music
+
+cogs = [music]
+
+client = commands.Bot(command_prefix='/', intents = discord.Intents.all())
+
+for i in range(len(cogs)):
+    cogs[i].setup(client)
+
+client.run("OTY4NTYyMTc5ODQ0NTA1NjQw.Ymgp0g.veq5HizVcXu_mV60ctXJup7NigA")
 
 
-def main():
-    bot = Bot()
-    TOKEN = os.environ.get("TOKEN")
-    keep_alive()
-    bot.run(TOKEN)
 
-if __name__=="__main__":
-    main()
+
---
+++ music.py
@@ -0,0 +1,71 @@
+import discord
+from discord.ext import commands
+import youtube_dl
+
+class music(commands.Cog):
+    def __init__(self,client):
+        self.client = client
+        
+    @commands.command()
+    async def join(self, ctx):
+        if ctx.author.voice is None:
+            await ctx.senc("You are not in a voice channel")
+        voice_chanel = ctx.author.voice.channel
+        if ctx.voice_client is None:
+            await voice_chanel.connect()
+        else:
+            await ctx.voice_client.move_to(voice_chanel)
+    
+    @commands.command()
+    async def disconnect(self, ctx):
+        await ctx.voice_client.disconnect()
+        
+    @commands.command()
+    async def play(self,ctx,url):
+        if ctx.voice_client is not None:
+          ctx.voice_client.stop()
+        FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5','options' : '-vn'}
+        YDL_OPTIONS = {'format' : 'bestaudio'}
+        vc = ctx.voice_client
+        
+        with youtube_dl.YoutubeDL(YDL_OPTIONS) as ydl:
+            info = ydl.extract_info(url,download=False)
+            url2 = info['formats'][0]['url']
+            source = await discord.FFmpegOpusAudio.from_probe(url2, **FFMPEG_OPTIONS)
+            vc.play(source)
+        
+    @commands.command()
+    async def pause(self, ctx):
+        voice = ctx.voice_client
+        if voice.is_playing():
+            voice.pause()
+        else:
+            await ctx.send('Currently no audio is playing.')
+        
+    @commands.command()
+    async def resume(self, ctx):
+        voice = ctx.voice_client
+            
+        if voice.is_paused():
+            voice.resume()
+        else:
+            await ctx.send('The audio is not paused.')
+            
+    @commands.command()
+    async def stop(self,ctx):
+        voice = ctx.voice_client
+        voice.stop()
+        await ctx.send('Bot stopped')
+        
+    @commands.command()
+    async def leave(self,ctx):
+        voice = ctx.voice_client
+        if voice.is_connected():
+            await voice.disconnect()
+            await ctx.send('Music stopped.')  
+        else:
+            await ctx.send('The bot is not connected to a voice channel.')  
+    
+    
+def setup(client):
+    client.add_cog(music(client))
